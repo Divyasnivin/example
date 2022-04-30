7f5ff70c71051041c8c73faf72d2ba1c3f9ef1c6
iCriis390 <97095454+iCriis390@users.noreply.github.com>
2022-05-01 01:31:25 +0200

    Update seek.py

--- YukkiMusic/plugins/admins/seek.py
+++ YukkiMusic/plugins/admins/seek.py
@@ -22,7 +22,7 @@ SEEK_COMMAND = get_command("SEEK_COMMAND")
 
 
 @app.on_message(
-    filters.command(SEEK_COMMAND)
+    command(SEEK_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
