285421a5af0edb991647195b90c0677883c5ea40
iCriis390 <97095454+iCriis390@users.noreply.github.com>
2022-05-01 01:32:54 +0200

    Update unmute.py

--- YukkiMusic/plugins/admins/unmute.py
+++ YukkiMusic/plugins/admins/unmute.py
@@ -22,7 +22,7 @@ UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
 
 
 @app.on_message(
-    filters.command(UNMUTE_COMMAND)
+    command(UNMUTE_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
