aecd9a19ed366aba15747fe44d48641e2df24422
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-05-02 02:59:50 +0300

    Create  بندر.py

---
+++ YukkiMusic/plugins/play/بندر.py
@@ -0,0 +1,27 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(
+    command(["نيلسون"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/e16471c46755ec36e98db.jpg",
+        caption=f"""-    Dev  .""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        " نيلسون ", url=f"https://t.me/t3ttt"),
+                
+                ],
+            ]
+        ),
+    )
