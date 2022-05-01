e8d8c94703b911a75baaded5830d9ba856fe878f
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-05-02 02:19:33 +0300

    Create اوامربوت.py

---
+++ YukkiMusic/plugins/play/اوامربوت.py
@@ -0,0 +1,29 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(
+    command(["الاوامر","اوامر"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/43c878e3954507d991e7d.jpg",
+        caption=f""" — لاوامر التشغيل بالقروبات ⇐  ①\n\n — لاوامر التشغيل بالقناة  ⇐  ②""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                      InlineKeyboardButton(
+                        "①", callback_data=f"tt"),
+                    InlineKeyboardButton(
+                        "②", callback_data=f"ddd"),
+                    
+                                                 ],
+            ]
+        ),
+    ) 
