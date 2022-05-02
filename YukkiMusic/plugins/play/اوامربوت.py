602a3d04bff9b3e9c6f5fb0cce75b17020dd61fb
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-05-03 02:02:03 +0300

    Update اوامربوت.py

--- YukkiMusic/plugins/play/اوامربوت.py
+++ YukkiMusic/plugins/play/اوامربوت.py
@@ -14,7 +14,7 @@ from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, ap
 async def khalid(client: Client, message: Message):
     return await message.reply_text(
         "** اوامر المكالمات**" )
-        caption=f""" — لاوامر التشغيل بالقروبات ⇐  ①\n\n — لاوامر التشغيل بالقناة  ⇐  ②""",
+        caption=f""" — لاوامر التشغيل بالقروبات ⇐  ①\n\n — لاوامر التشغيل بالقناة  ⇐  ②"""
         reply_markup=InlineKeyboardMarkup(
             [
                 [
