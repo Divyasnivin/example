b3baa9ca4900118a4819be1d50abbdacbd9170ce
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-05-03 07:28:04 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -28,6 +28,9 @@ def start_pannel(_):
     ]
     InlineKeyboardButton(
                         text=_["S_B_9"], callback_data="close"),
+                ),
+        ],
+    ]
                     InlineKeyboardButton(
                         text=_["S_B_10"], callback_data="back"),
                     ),
