f91b5c061714ba9222b1e5a1d9499ff042a0c165
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-05-03 07:37:54 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -25,7 +25,7 @@ def start_pannel(_):
                 text=_["S_B_2"], callback_data="settings_helper"
             ),
         ],
-    )
+    ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
             [
