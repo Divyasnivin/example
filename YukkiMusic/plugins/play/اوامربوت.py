c1505b8be85f70c64c1661689d9cd5d6386b9e79
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-05-03 09:12:42 +0300

    Update اوامربوت.py

--- YukkiMusic/plugins/play/اوامربوت.py
+++ YukkiMusic/plugins/play/اوامربوت.py
@@ -12,8 +12,8 @@ from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, ap
     & ~filters.edited
 )
 async def khalid(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"{PING_IMG_URL}",
+    await message.reply_text("تم تفعيل وضع الصيانة"
+                             )
         caption=f""" — لاوامر التشغيل بالقروبات ⇐  ①\n\n — لاوامر التشغيل بالقناة  ⇐  ②""",
         reply_markup=InlineKeyboardMarkup(
             [
