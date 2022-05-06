853e6cead3712e879674faaf5621aba318e2e7aa
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-05-06 08:40:39 +0300

    Update اوامربوت.py

--- YukkiMusic/plugins/play/اوامربوت.py
+++ YukkiMusic/plugins/play/اوامربوت.py
@@ -7,7 +7,7 @@ from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, ap
 
 
 @app.on_message(
-    command(["المكالمه","اوامر"])
+    command(["المكالمه","مكالمه"])
     & filters.group
     & ~filters.edited
 )
